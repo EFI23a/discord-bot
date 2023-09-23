a4baa8a75416fffed9db3c88a6330b1ccd39b40d
Jamey420 <jameyknabbert@gmail.com>
2023-09-23 21:25:14 +0200

    reformatted code

--- src/main/java/de/efi23a/bot/database/MongoConfig.java
+++ src/main/java/de/efi23a/bot/database/MongoConfig.java
@@ -4,18 +4,17 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import java.util.Collection;
+import java.util.Collections;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import java.util.Collection;
-import java.util.Collections;
 
 /*
-*
-* MongoDB Config
-* https://www.baeldung.com/spring-data-mongodb-tutorial
-*
-* */
+ * MongoDB Config
+ * https://www.baeldung.com/spring-data-mongodb-tutorial
+ * */
+
 @Configuration
 @EnableMongoRepositories(basePackages = "de.efi23a.bot.database.repository")
 public class MongoConfig extends AbstractMongoClientConfiguration {
@@ -30,7 +29,8 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
 
   @Override
   public MongoClient mongoClient() {
-    ConnectionString connectionString = new ConnectionString(System.getenv(CONNECTION_STRING_ENV_VARIABLE));
+    ConnectionString connectionString =
+        new ConnectionString(System.getenv(CONNECTION_STRING_ENV_VARIABLE));
     MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
         .applyConnectionString(connectionString)
         .build();
